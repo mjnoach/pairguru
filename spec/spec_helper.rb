d5f901eaeb5bbe4a7728ddd6be4724ff2b2a33f4
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-11-12 20:16:42 +0200

    Print path of currently running test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,15 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.before :each do |example|
+    path = example.metadata[:example_group][:file_path]
+    curr_path = config.instance_variable_get(:@curr_file_path)
+    if (curr_path.nil? || path != curr_path)
+      config.instance_variable_set(:@curr_file_path, path)
+      puts "", path
+    end
+  end
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
