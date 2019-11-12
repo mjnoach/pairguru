02f3a59aa81546a9e8a257e3a7cab7110c90c069
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-11-12 20:17:49 +0200

    Create MovieFetcher service for retrieving data from movies api

---
+++ app/services/movie_fetcher.rb
@@ -0,0 +1,10 @@
+class MovieFetcher
+  @@api_base = "https://pairguru-api.herokuapp.com"
+
+  def call(movie_title)
+    movie_url = @@api_base + "/api/v1/movies/" + URI.escape(movie_title)
+    data = HTTParty.get(movie_url).parsed_response['data']['attributes']
+    data['poster'] = @@api_base + data['poster']
+    data
+  end
+end
