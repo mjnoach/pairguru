b81e6ad40e13a41ccaa1e786f5e9a3e438158ebc
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-11-13 23:03:06 +0200

    Create comment model with proper associations

---
+++ db/migrate/20191005234559_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.references :user, foreign_key: true
+      t.references :movie, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :comments, [:user_id, :movie_id], :unique => true
+  end
+end
