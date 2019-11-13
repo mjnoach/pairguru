b5d40494067851461f3dabf6eda04dd5b6500043
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-11-13 02:12:09 +0200

    Fix: Add movie api data caching

--- app/services/movie_fetcher.rb
+++ app/services/movie_fetcher.rb
@@ -3,10 +3,8 @@ class MovieFetcher
 
   def call(movie_title)
     movie_url = @@api_base + "/api/v1/movies/" + URI.escape(movie_title)
-    data = Rails.cache.fetch(movie_url) do
-      HTTParty.get(movie_url).parsed_response['data']['attributes']
-    end
     data = HTTParty.get(movie_url).parsed_response['data']['attributes']
+    puts data['poster']
     data['poster'] = @@api_base + data['poster']
     data
   end
