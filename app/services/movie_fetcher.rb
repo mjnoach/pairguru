c2e9c99f88cd4fa75267b5b232c1c1ade6e08919
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-11-14 20:52:45 +0200

    Rename MovieFetcher api_base class var to a constant

--- app/services/movie_fetcher.rb
+++ app/services/movie_fetcher.rb
@@ -1,11 +1,10 @@
 class MovieFetcher
-  @@api_base = "https://pairguru-api.herokuapp.com"
+  API_BASE = "https://pairguru-api.herokuapp.com"
 
   def call(movie_title)
-    movie_url = @@api_base + "/api/v1/movies/" + URI.escape(movie_title)
+    movie_url = API_BASE + "/api/v1/movies/" + URI.escape(movie_title)
     data = HTTParty.get(movie_url).parsed_response['data']['attributes']
-    puts data['poster']
-    data['poster'] = @@api_base + data['poster']
+    data['poster'] = API_BASE + data['poster']
     data
   end
 end
