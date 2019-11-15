6b5adfa9ffd96718d223d1c3ed483a3a82fb6404
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-11-15 18:14:00 +0100

    Slims out Comments controller by moving comment creation to CommentCreator service

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    comment = current_user.comments.new(comment_params)
-    comment.movie = Movie.find(params[:movie_id])
-    if !comment.save
+    comment = CommentCreator.new(current_user, params[:movie_id], comment_params).call
+    if comment.persisted?
+      redirect_back(fallback_location: root_path)
+    else
       flash[:comment_errors] = comment.errors.full_messages
+      redirect_back(fallback_location: root_path, status: :bad_request)
     end
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
