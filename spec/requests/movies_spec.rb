c4f91bb9fcd39939d104a815fa18ac9bc9d8e7f8
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-11-17 21:51:25 +0100

    Add 'fetch_api_data' stub in movies spec

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -4,6 +4,10 @@ describe "Movies requests", type: :request do
   let!(:movies) { create_list(:movie, 10) }
 
   describe "movie page" do
+    before(:each) do
+      allow_any_instance_of(Movie).to receive(:fetch_api_data).and_return(create(:movie))
+    end
+    
     it "displays new comment form" do
       visit "/movies/" + movies.sample.id.to_s
       expect(page).to have_selector("form")
